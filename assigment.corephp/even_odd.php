79f05b9f13ecb2f0dd7a21ec13ea3bc5c5c9f457
ayushi8899 <112918540+ayushi8899@users.noreply.github.com>
2023-02-16 09:30:32 +0530

    Add files via upload

---
+++ assigment.corephp/even_odd.php
@@ -0,0 +1,18 @@
+<?php
+
+using namespace std;
+ 
+int main()
+{
+    int n = 6;
+    string arr[] = {"even", "odd"};
+ 
+    // using modulo operator
+    cout << arr[n % 2];
+ 
+    return 0;
+}
+?>
+
+
+
